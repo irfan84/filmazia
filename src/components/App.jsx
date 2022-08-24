e7cb1e32aec93f90f8c182ad6cfdbed1c34efe91
Irfan Shakeel <mirfanshakeel84@gmail.com>
2022-08-24 12:45:43 +0500

    Project Structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Movies, MovieInformation, Actors, Profile, NavBar } from './index'
+import useStyles from './styles'
 
 const App = () => {
+ const classes = useStyles();
   return (
-    <div>
-      App
+    <div className={classes.root}>
+    <CssBaseline />
+    <NavBar />
+      <main className={classes.content}>
+      <div className={classes.toolbar}>
+      <Router>
+      <Routes>
+        <Route exact path="/movie/:id" element={<MovieInformation />} />
+        <Route exact path="/actors" element={<Actors />} />
+        <Route exact path="/" element={<Movies />} />
+        <Route exact path="/profile/:id" element={<Profile />} />
+      </Routes>
+      </Router>
+</div>
+      </main>
     </div>
   )
 }
